f11f640ea4d25fd31a81cab1632a7bdb81fc5125
ramkaran saini <karansainikaran30@gmail.com>
2026-07-30 21:58:59 +0530

    Add multiply and divide methods

--- calculator.py
+++ calculator.py
@@ -10,3 +10,10 @@ class Calculator:
 calc = Calculator()
 print("Addition:", calc.add(10, 5))
 print("Subtraction:", calc.subtract(10, 5))
+#Ramaran saini
+
+def multiply(self, a, b):
+        return a * b
+
+def divide(self, a, b):
+        return a / b
