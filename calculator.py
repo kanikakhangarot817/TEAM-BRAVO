bb01be91b16cdd567645f9b2c14ec67e263fc01e
Kanika Khangarot <kanikakhangarot817@gmail.com>
2026-07-30 21:24:48 +0530

    Add calculator class with add and subtract methods

---
+++ calculator.py
@@ -0,0 +1,12 @@
+class Calculator:
+    # Kanika
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+
+calc = Calculator()
+print("Addition:", calc.add(10, 5))
+print("Subtraction:", calc.subtract(10, 5))
